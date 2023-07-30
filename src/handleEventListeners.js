16052d0d3c972919ac68333c7dbbac898b1406d5
Ivelin Iliev <73820008+ivooo369@users.noreply.github.com>
2023-07-30 17:42:26 +0300

    Add styling to the creation window

--- src/handleEventListeners.js
+++ src/handleEventListeners.js
@@ -1,5 +1,5 @@
 const homeButton = document.querySelector('#home-button');
-const menuButton = document.querySelector('#menu-button');
+const menuToggle = document.querySelector('#menu-toggle');
 const allTasksPage = document.querySelector('#all-tasks-page');
 const todayTasksPage = document.querySelector('#today-tasks-page');
 const weekTasksPage = document.querySelector('#week-tasks-page');
@@ -10,6 +10,16 @@ const weekTasksButton = document.querySelector('#week-tasks-button');
 const allNotesButton = document.querySelector('#all-notes-button');
 const menuContainer = document.querySelector('.menu-container');
 const pages = document.querySelectorAll('.pages');
+const createButton = document.querySelector('.create-button');
+const creationWindow = document.querySelector('.creation-window');
+const overlay = document.querySelector(".overlay");
+const creationWindowCloseButton = document.querySelector('#creation-window-close-button');
+const todoButton = document.querySelector('#todo-button');
+const projectButton = document.querySelector('#project-button');
+const noteButton = document.querySelector('#note-button');
+const todoForm = document.querySelector('#todo-form');
+const projectForm = document.querySelector('#project-form');
+const noteForm = document.querySelector('#note-form');
 
 export function handleEventListeners() {
     homeButton.addEventListener('click', () => {
@@ -19,7 +29,7 @@ export function handleEventListeners() {
         allNotesPage.style.display = 'none';
     });
 
-    menuButton.addEventListener('click', () => {
+    menuToggle.addEventListener('click', () => {
         menuContainer.classList.toggle('hidden-menu');
         pages.forEach(page => {
             page.classList.toggle('pages-full');
@@ -27,10 +37,10 @@ export function handleEventListeners() {
     });
 
     allTasksButton.addEventListener('click', () => {
-        allTasksButton.classList.add('active');
-        todayTasksButton.classList.remove('active');
-        weekTasksButton.classList.remove('active');
-        allNotesButton.classList.remove('active');
+        allTasksButton.classList.add('menu-button-active');
+        todayTasksButton.classList.remove('menu-button-active');
+        weekTasksButton.classList.remove('menu-button-active');
+        allNotesButton.classList.remove('menu-button-active');
         allTasksPage.style.display = 'flex';
         todayTasksPage.style.display = 'none';
         weekTasksPage.style.display = 'none';
@@ -38,10 +48,10 @@ export function handleEventListeners() {
     });
 
     todayTasksButton.addEventListener('click', () => {
-        todayTasksButton.classList.add('active');
-        allTasksButton.classList.remove('active');
-        weekTasksButton.classList.remove('active');
-        allNotesButton.classList.remove('active');
+        todayTasksButton.classList.add('menu-button-active');
+        allTasksButton.classList.remove('menu-button-active');
+        weekTasksButton.classList.remove('menu-button-active');
+        allNotesButton.classList.remove('menu-button-active');
         todayTasksPage.style.display = 'flex';
         allTasksPage.style.display = 'none';
         weekTasksPage.style.display = 'none';
@@ -49,10 +59,10 @@ export function handleEventListeners() {
     });
 
     weekTasksButton.addEventListener('click', () => {
-        weekTasksButton.classList.add('active');
-        allTasksButton.classList.remove('active');
-        todayTasksButton.classList.remove('active');
-        allNotesButton.classList.remove('active');
+        weekTasksButton.classList.add('menu-button-active');
+        allTasksButton.classList.remove('menu-button-active');
+        todayTasksButton.classList.remove('menu-button-active');
+        allNotesButton.classList.remove('menu-button-active');
         weekTasksPage.style.display = 'flex';
         todayTasksPage.style.display = 'none';
         allTasksPage.style.display = 'none';
@@ -60,13 +70,56 @@ export function handleEventListeners() {
     });
 
     allNotesButton.addEventListener('click', () => {
-        allNotesButton.classList.add('active');
-        allTasksButton.classList.remove('active');
-        todayTasksButton.classList.remove('active');
-        weekTasksButton.classList.remove('active');
+        allNotesButton.classList.add('menu-button-active');
+        allTasksButton.classList.remove('menu-button-active');
+        todayTasksButton.classList.remove('menu-button-active');
+        weekTasksButton.classList.remove('menu-button-active');
         allNotesPage.style.display = 'flex';
         weekTasksPage.style.display = 'none';
         todayTasksPage.style.display = 'none';
         allTasksPage.style.display = 'none';
+    });
+
+    createButton.addEventListener('click', () => {
+        creationWindow.classList.add('open-creation-window');
+        todoButton.classList.add('selection-button-active');
+        overlay.style.display = 'block';
+    });
+
+    creationWindowCloseButton.addEventListener('click', () => {
+        creationWindow.classList.remove('open-creation-window');
+        projectButton.classList.remove('selection-button-active');
+        noteButton.classList.remove('selection-button-active');
+        overlay.style.display = 'none';
+        todoForm.style.display = 'flex';
+        projectForm.style.display = 'none';
+        noteForm.style.display = 'none';
+    });
+
+    todoButton.addEventListener('click', () => {
+        todoForm.style.display = 'flex';
+        projectForm.style.display = 'none';
+        noteForm.style.display = 'none';
+        todoButton.classList.add('selection-button-active');
+        projectButton.classList.remove('selection-button-active');
+        noteButton.classList.remove('selection-button-active');
+    });
+
+    projectButton.addEventListener('click', () => {
+        projectForm.style.display = 'flex';
+        todoForm.style.display = 'none';
+        noteForm.style.display = 'none';
+        projectButton.classList.add('selection-button-active');
+        todoButton.classList.remove('selection-button-active');
+        noteButton.classList.remove('selection-button-active');
+    });
+
+    noteButton.addEventListener('click', () => {
+        noteForm.style.display = 'flex';
+        todoForm.style.display = 'none';
+        projectForm.style.display = 'none';
+        noteButton.classList.add('selection-button-active');
+        todoButton.classList.remove('selection-button-active');
+        projectButton.classList.remove('selection-button-active');
     });
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,40 @@
 import './style.css';
+import Icon from './images/tab-icon.png';
 import { createImages } from './domManipulation';
 import { handleEventListeners } from './handleEventListeners';
+
+const head = document.querySelector('head');
+const tabIcon = document.createElement('link');
+tabIcon.rel = 'icon';
+tabIcon.href = Icon;
+head.appendChild(tabIcon);
 
 createImages();
 handleEventListeners();
 
-export const addTask = (title, description, dueDate, project, priority, isDone) => {
-    return { title, description, dueDate, project, priority, isDone };
-};
 
-export const inbox = {
-    tasks: []
-};
 
-console.log(inbox.tasks);
+// const addTask = (title, description, dueDate, project, priority) => {
+//     return { title, description, dueDate, project, priority };
+// };
+
+// const inbox = {
+//     tasks: []
+// };
+
+// const array = inbox.tasks;
+
+// const task1 = addTask('demo', 'd', 324, 'fdsf', 'low');
+// const task2 = addTask('deo', 'gdfgd', 343524, 'fd', 'l');
+// array.push(task1);
+// array.push(task2);
+
+// const lists = document.querySelector('.lists');
+
+// array.forEach(e => {
+//     const div = document.createElement('div');
+//     div.textContent = `${e.title}, ${e.description}, ${e.dueDate}, ${e.project}, ${e.priority}`;
+//     lists.appendChild(div);
+// });
+
+// console.log(array);
